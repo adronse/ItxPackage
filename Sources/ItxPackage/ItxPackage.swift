e3f01208c4a14e961ccab15cd88d88a49d52e60a
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-15 16:19:02 +0100

    testouille

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -1,2 +1,15 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// Sources/MySwiftPackage/MySwiftPackage.swift
+
+import UIKit
+
+public struct MySwiftPackage {
+    public init() {}
+
+    public func sayHello() {
+        print("Hello from Itx Package!")
+        let alert = UIAlertController(title: "This is a nice alert bro", message: "Alert !!!!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+        
+    }
+}
