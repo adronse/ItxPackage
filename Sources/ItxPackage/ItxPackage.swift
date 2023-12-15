c625eaf4be1722703a168a15c238c8fa53806878
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-15 18:14:48 +0100

    input fields test

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -15,22 +15,54 @@ public struct MySwiftPackage {
         }
     }
     
-    
-    
     public class ScreenshotObserver {
         @objc static func detectScreenshot() {
-            
-
-           lazy var SendIssueBox = UIView()
+            // Create SendIssueBox view
+            let sendIssueBox = UIView()
                 .with(\.backgroundColor, value: .red)
+                .with(\.translatesAutoresizingMaskIntoConstraints, value: false)
             
-            // Retrieve the top-most view controller to present the alert
+            // Create title input field
+            let titleTextField = UITextField()
+                .with(\.placeholder, value: "Title")
+                .with(\.borderStyle, value: .roundedRect)
+                .with(\.translatesAutoresizingMaskIntoConstraints, value: false)
+            
+            // Create description input field
+            let descriptionTextField = UITextField()
+                .with(\.placeholder, value: "Description")
+                .with(\.borderStyle, value: .roundedRect)
+                .with(\.translatesAutoresizingMaskIntoConstraints, value: false)
+            
+            // Retrieve the top-most view controller
             if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
-                topViewController.view.addSubview(SendIssueBox)
+                // Add SendIssueBox to the top view controller's view
+                topViewController.view.addSubview(sendIssueBox)
                 
-                SendIssueBox.snp.makeConstraints { make in
-                    make.center.equalTo(topViewController.view)
-                    make.width.height.equalTo(50)
+                // Add title and description text fields to SendIssueBox
+                sendIssueBox.addSubview(titleTextField)
+                sendIssueBox.addSubview(descriptionTextField)
+                
+                // Set up constraints for SendIssueBox
+                sendIssueBox.snp.makeConstraints { make in
+                    make.centerX.equalToSuperview()
+                    make.centerY.equalToSuperview()
+                    make.width.equalTo(300) // Adjust the width as needed
+                    make.height.equalTo(200) // Adjust the height as needed
+                }
+                
+                // Set up constraints for titleTextField
+                titleTextField.snp.makeConstraints { make in
+                    make.top.equalTo(sendIssueBox).offset(20)
+                    make.leading.trailing.equalTo(sendIssueBox).inset(20)
+                    make.height.equalTo(30)
+                }
+                
+                // Set up constraints for descriptionTextField
+                descriptionTextField.snp.makeConstraints { make in
+                    make.top.equalTo(titleTextField.snp.bottom).offset(10)
+                    make.leading.trailing.equalTo(sendIssueBox).inset(20)
+                    make.height.equalTo(30)
                 }
             }
         }
