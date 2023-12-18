19b2070b9d211bb8fda7e17d803a0cde5cb245cb
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 11:06:11 +0100

    snp

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -64,6 +64,19 @@ public struct MySwiftPackage {
                 .with(\.textColor, value: UIColor.gray)
                 .with(\.numberOfLines, value: 0) // Allow multiline text
             
+            // Create buttons for the second row ("Suggest an improvement")
+            let suggestImprovementButton = UIButton(type: .system)
+            // suggestImprovementButton.addTarget(self, action: #selector(suggestImprovementButtonTapped), for: .touchUpInside)
+
+            let suggestImprovementTitleLabel = UILabel()
+                .with(\.text, value: "Suggest an improvement")
+                .with(\.textColor, value: UIColor.white)
+
+            let suggestImprovementDescriptionLabel = UILabel()
+                .with(\.text, value: "New ideas or desired enhancements for this app")
+                .with(\.textColor, value: UIColor.gray)
+                .with(\.numberOfLines, value: 0) // Allow multiline text
+
             let separator2 = UIView.separator(color: UIColor.gray)
 
             
@@ -120,20 +133,43 @@ public struct MySwiftPackage {
                     make.height.equalTo(1)
                 }
                 
-                suggestImprovementTitle.snp.makeConstraints { make in
+                reportBugButton.snp.makeConstraints { make in
                     make.top.equalTo(separator1.snp.bottom)
                     make.leading.trailing.equalToSuperview()
                     make.bottom.equalTo(separator1.snp.bottom)
                 }
 
-                suggestImprovementDescription.snp.makeConstraints { make in
+                reportBugTitleLabel.snp.makeConstraints { make in
                     make.top.equalTo(reportBugButton).offset(10)
                     make.leading.equalTo(reportBugButton).offset(10)
                 }
                 
+                // Add buttons and separator to the sendIssueBox
+                sendIssueBox.addSubview(suggestImprovementButton)
+                sendIssueBox.addSubview(suggestImprovementTitleLabel)
+                sendIssueBox.addSubview(suggestImprovementDescriptionLabel)
+                sendIssueBox.addSubview(separator2)
+
+                // Position the buttons and separator for the second row
+                suggestImprovementButton.snp.makeConstraints { make in
+                    make.top.equalTo(separator1.snp.bottom)
+                    make.leading.trailing.equalToSuperview()
+                    make.bottom.equalTo(separator2.snp.bottom)
+                }
+
+                suggestImprovementTitleLabel.snp.makeConstraints { make in
+                    make.top.equalTo(suggestImprovementButton).offset(10)
+                    make.leading.equalTo(suggestImprovementButton).offset(10)
+                }
+
+                suggestImprovementDescriptionLabel.snp.makeConstraints { make in
+                    make.top.equalTo(suggestImprovementTitleLabel.snp.bottom).offset(5)
+                    make.leading.trailing.equalTo(suggestImprovementButton).inset(10)
+                }
+
                 separator2.snp.makeConstraints { make in
-                    make.top.equalTo(suggestImprovementDescription.snp.bottom).offset(20)
-                    make.leading.trailing.equalTo(suggestImprovementDescription)
+                    make.top.equalTo(suggestImprovementDescriptionLabel.snp.bottom).offset(20)
+                    make.leading.trailing.equalTo(suggestImprovementButton)
                     make.height.equalTo(1)
                 }
             }
