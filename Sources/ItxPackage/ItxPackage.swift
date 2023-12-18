3bb3736af61cff80c2686ef605f5bc42e5f3bd37
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 16:11:38 +0100

    test

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -17,33 +17,65 @@ public struct MySwiftPackage {
         }
     }
 }
-    
 
-    public class ScreenshotObserver {
+
+public class ScreenshotObserver {
+    
+    
+    @objc static func didTapCancelButton()
+    {
+        
+    }
+    
+    
+    @objc static func detectScreenshot() {
         
         
-        @objc static func didTapCancelButton()
-        {
+        lazy var imageView = UIImageView()
+        
+        
+        // Retrieve the top-most view controller
+        if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
             
-        }
-        
-        
-        @objc static func detectScreenshot() {
-        
             
-            // Retrieve the top-most view controller
-            if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
-                
-                let issueBox = IssueBoxView()
-                topViewController.view.addSubview(issueBox)
-                
-                issueBox.snp.makeConstraints { make in
-                    make.centerX.equalToSuperview()
-                    make.centerY.equalToSuperview()
-                    make.width.equalTo(300)
-                    make.height.equalTo(250)
-                    
+            if let screenshot = captureScreen(view: topViewController.view) {
+                // Do something with the screenshot, e.g., save it, display it, etc.
+                // For now, let's print the image data size
+                if let jpegData = screenshot.jpegData(compressionQuality: 1.0) {
+                    print("Captured screenshot with size: \(jpegData.count) bytes")
                 }
+                
+                topViewController.view.addSubview(imageView)
+                imageView.image = screenshot
+                
+                imageView.snp.makeConstraints { make in
+                    make.edges.equalToSuperview()
+                }
+            
             }
+            
+            
+            
+//            let issueBox = IssueBoxView()
+//            topViewController.view.addSubview(issueBox)
+//            
+//            issueBox.snp.makeConstraints { make in
+//                make.centerX.equalToSuperview()
+//                make.centerY.equalToSuperview()
+//                make.width.equalTo(300)
+//                make.height.equalTo(250)
+//                
+//            }
         }
     }
+    
+    static func captureScreen(view: UIView) -> UIImage? {
+        // Capture the screen as an image
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
+        view.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let screenshot = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return screenshot
+    }
+}
