bef503d631e78f84a12898187c061a563bbb4be3
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 12:20:10 +0100

    close button

--- Sources/ItxPackage/IssueCreationViewController.swift
+++ Sources/ItxPackage/IssueCreationViewController.swift
@@ -1,36 +1,39 @@
-//
-//  File.swift
-//  
-//
-//  Created by Adrien Ronse on 18/12/2023.
-//
-
 import Foundation
 import UIKit
 
-public class IssueCreationViewController : UIViewController
-{
+public class IssueCreationViewController: UIViewController {
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Report a bug"
         self.view.backgroundColor = UIColor.from(hex: "#292A2F")
         configureUI()
+        addCloseButton()
     }
-    
-    
+
     private lazy var IssueCreationTitle = UILabel()
         .with(\.text, value: "cool mec")
         .with(\.textColor, value: UIColor.from(hex: "#11590D"))
-    
-    
-    private func configureUI()
-    {
 
+    private func configureUI() {
         view.addSubview(IssueCreationTitle)
-        
-        
+
         IssueCreationTitle.snp.makeConstraints { make in
             make.center.equalToSuperview()
         }
+    }
+
+    private func addCloseButton() {
+        let closeButton = UIButton(type: .custom)
+        closeButton.setImage(UIImage(named: "closeIcon"), for: .normal)
+        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
+
+        let closeBarButtonItem = UIBarButtonItem(customView: closeButton)
+        navigationItem.leftBarButtonItem = closeBarButtonItem
+    }
+
+    @objc private func closeButtonTapped() {
+        // Handle close button tap action here
+        navigationController?.popViewController(animated: true)
     }
 }
