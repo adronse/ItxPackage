7884b91d5e99a66b833de26ca48a884f0256fe15
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 13:56:56 +0100

    pan gesture

--- Sources/ItxPackage/IssueCreationViewController.swift
+++ Sources/ItxPackage/IssueCreationViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 import Photos
 
 public class IssueCreationViewController: UIViewController {
-
+    
+    private var panGestureRecognizer: UIPanGestureRecognizer!
+    private var animator: UIViewPropertyAnimator!
+    
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Report a bug"
         self.view.backgroundColor = UIColor.from(hex: "#292A2F")
         configureUI()
         displayLatestScreenshot()
+        
+        panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        view.addGestureRecognizer(panGestureRecognizer)
     }
     
     private lazy var emailFieldTitle: UILabel = {
@@ -38,7 +45,7 @@ public class IssueCreationViewController: UIViewController {
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
     private func configureUI() {
         view.addSubview(emailFieldTitle)
         view.addSubview(emailField)
@@ -69,25 +76,54 @@ public class IssueCreationViewController: UIViewController {
             screenshotImageView.image = latestScreenshot
         }
     }
-
+    
     private func fetchLatestScreenshot() -> UIImage? {
         let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
         let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
-
+        
         guard let latestAsset = fetchResult.firstObject else {
             return nil
         }
-
+        
         let requestOptions = PHImageRequestOptions()
         requestOptions.isSynchronous = true
-
+        
         var latestScreenshot: UIImage?
-
+        
         PHImageManager.default().requestImage(for: latestAsset, targetSize: CGSize(width: 200, height: 200), contentMode: .aspectFit, options: requestOptions) { (image, _) in
             latestScreenshot = image
         }
-
+        
         return latestScreenshot
+    }
+    
+    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
+        let translation = gesture.translation(in: view)
+        
+        switch gesture.state {
+        case .began:
+            animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut, animations: {
+                self.view.frame.origin.y = 0
+            })
+            animator.pauseAnimation()
+        case .changed:
+            let offsetY = max(0, translation.y)
+            let fraction = offsetY / view.bounds.height
+            animator.fractionComplete = fraction
+        case .ended, .cancelled:
+            let velocity = gesture.velocity(in: view).y
+            if velocity > 0 || animator.fractionComplete > 0.5 {
+                animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+            } else {
+                animator.stopAnimation(true)
+                animator.addAnimations {
+                    self.view.frame.origin.y = -self.view.bounds.height
+                }
+                animator.startAnimation()
+            }
+        default:
+            break
+        }
     }
 }
