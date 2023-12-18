7a7b81974e8e94da396bea415c5a6845c378bb45
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 17:39:27 +0100

    refac ui

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -4,8 +4,6 @@ import UIKit
 import SnapKit
 
 
-
-
 public class ImageViewController : UIViewController {
     
     private let imageView: UIImageView
@@ -34,11 +32,84 @@ public class ImageViewController : UIViewController {
 }
 
 
-import UIKit
+class ReportButton: UIView {
+    
+    private let title: String
+    private let reportButtonDescription: String
+    
+    // Designated initializer
+    init(title: String, reportButtonDescription: String) {
+        self.title = title
+        self.reportButtonDescription = reportButtonDescription
+        super.init(frame: .zero) // You can set the frame here or customize it as needed
+        setupUI()
+        setupGesture()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupGesture()
+    {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+        addGestureRecognizer(tapGesture)
+        isUserInteractionEnabled = true
+    }
+    
+    private func setupUI() {
+        
+        let buttonView = UIView()
+        
+        addSubview(buttonView)
+        
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        buttonView.addSubview(titleLabel)
+        
+        let descLabel = UILabel()
+        descLabel.text = reportButtonDescription
+        buttonView.addSubview(descLabel)
+        
+        buttonView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(buttonView.snp.top).offset(5)
+        }
+        
+        descLabel.snp.makeConstraints { make in
+            make.leading.equalTo(titleLabel)
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+        }
+        
+    }
+    
+    @objc private func didTap()
+    {
+        
+    }
+}
 
-import UIKit
 
 class DummyController: UIViewController {
+    
+    
+    private let imageView: UIImageView
+    
+    
+    init(imageView: UIImageView) {
+        self.imageView = imageView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
     let popupView: UIView = {
         let view = UIView()
@@ -52,13 +123,18 @@ class DummyController: UIViewController {
     lazy var popupTitle = UILabel()
         .with(\.text, value: "Need help?")
         .with(\.textColor, value: UIColor.from(hex: "#dedfe0"))
+        .with(\.font, value: .systemFont(ofSize: 12))
     
+    
+    lazy var separator = UIView.separator(color: UIColor.from(hex: "#333333"))
+    
+    lazy var reportBugButton = ReportButton(title: "Report a bug", reportButtonDescription: "Something in the app is broken or doesn't work as expected")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Set the background color (optional)
-        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        view.backgroundColor = .clear
 
         // Add the popup view to the view hierarchy
         view.addSubview(popupView)
@@ -76,10 +152,22 @@ class DummyController: UIViewController {
 
     func configurePopupView() {
         popupView.addSubview(popupTitle)
+        popupView.addSubview(separator)
+        popupView.addSubview(reportBugButton)
         
         popupTitle.snp.makeConstraints { make in
             make.top.equalToSuperview()
             make.leading.equalTo(popupView.snp.leading).offset(5)
+        }
+        
+        separator.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(popupTitle.snp.bottom).offset(50)
+        }
+        
+        reportBugButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(separator.snp.bottom).offset(50)
         }
     }
 
@@ -105,7 +193,6 @@ public struct MySwiftPackage {
 
 public class ScreenshotObserver {
 
-    static var imageView = UIImageView()
 
     @objc static func didTapCancelButton() {
         // Handle cancel button tap if needed
@@ -122,7 +209,7 @@ public class ScreenshotObserver {
                 }
 
                 // Create and present the DummyController
-                let controller = DummyController()
+                let controller = DummyController(imageView: UIImageView(image: screenshot))
 
                 topViewController.present(controller, animated: true)
             }
