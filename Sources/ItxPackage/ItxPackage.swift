b4cf2c72042dae7d68d84f7756d6f611b1ee742b
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 11:37:27 +0100

    fix

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -3,10 +3,6 @@
 import UIKit
 import SnapKit
 
-public class ScreenshotObserver {
-    @objc static func detectScreenshot() {
-        // Handle screenshot detection
-        print("Screenshot detected!")
 
 
 public struct MySwiftPackage {
@@ -23,34 +19,6 @@ public struct MySwiftPackage {
 }
     
 
-struct MySwiftUIView_Previews: PreviewProvider {
-    @available(iOS 13.0.0, *)
-    static var previews: some View {
-        VStack(spacing: 20) {
-                    Text("Need help ?")
-                        .foregroundColor(.white)
-                        .font(.title)
-                    
-                    Rectangle()
-                        .frame(height: 1)
-                        .foregroundColor(Color.gray)
-                    
-                    // Add your buttons and UI components here
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        // Dismiss the SwiftUI view if needed
-                        UIApplication.shared.windows.first?.rootViewController?.dismiss(animated: true, completion: nil)
-                    }) {
-                        Text("Cancel")
-                            .foregroundColor(.white)
-                    }
-                }
-                .padding()
-                .frame(width: 300, height: 200)
-                .background(Color(UIColor.from(hex: "#41403F", alpha: 1)))
-                .cornerRadius(8)
     public class ScreenshotObserver {
         
         
@@ -190,4 +158,3 @@ struct MySwiftUIView_Previews: PreviewProvider {
             }
         }
     }
-}
