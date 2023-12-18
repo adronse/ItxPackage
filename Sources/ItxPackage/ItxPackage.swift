0cc25af5874a6c5f3b6e63875d6dbe3138dcca6a
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-18 11:45:54 +0100

    height

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -88,7 +88,7 @@ public struct MySwiftPackage {
                     make.centerX.equalToSuperview()
                     make.centerY.equalToSuperview()
                     make.width.equalTo(300)
-                    make.height.equalTo(300)
+                    make.height.equalTo(250)
                 }
 
                 titleLabel.snp.makeConstraints { make in
