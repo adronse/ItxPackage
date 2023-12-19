163b544cf80108acaf5f8f6f4e2bf9cbee12da78
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-19 13:44:50 +0100

    test

--- Sources/ItxPackage/ItxPackage.swift
+++ Sources/ItxPackage/ItxPackage.swift
@@ -32,85 +32,85 @@ public class ImageViewController : UIViewController {
 }
 
 
-public class ReportButton: UIView {
-    
-    private let title: String
-    private let reportButtonDescription: String
-    private let systemNameIcon: String
-    
-    // Designated initializer
-    init(title: String, reportButtonDescription: String, systemNameIcon: String) {
-        self.title = title
-        self.reportButtonDescription = reportButtonDescription
-        self.systemNameIcon = systemNameIcon
-        super.init(frame: .zero) // You can set the frame here or customize it as needed
-        setupUI()
-        setupGesture()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupGesture() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
-        addGestureRecognizer(tapGesture)
-        isUserInteractionEnabled = true
-    }
-    
-    private func setupUI() {
-        
-        let buttonView = UIView()
-        
-        addSubview(buttonView)
-        
-        let image: UIImageView = {
-            let icon = UIImageView()
-            if #available(iOS 13.0, *) {
-                icon.image = UIImage(systemName: systemNameIcon)
-            }
-            return icon
-        }()
-        
-        lazy var titleLabel = UILabel()
-            .with(\.text, value: title)
-            .with(\.textColor, value: UIColor.from(hex: "#bbbcbd"))
-            .with(\.font, value: .systemFont(ofSize: 16, weight: .bold))
-        
-        lazy var descLabel = UILabel()
-            .with(\.text, value: reportButtonDescription)
-            .with(\.textColor, value: UIColor.from(hex: "#bbbcbd"))
-            .with(\.numberOfLines, value: 0)
-            .with(\.font, value: .systemFont(ofSize: 12, weight: .light))
-        
-        buttonView.addSubview(titleLabel)
-        buttonView.addSubview(descLabel)
-        buttonView.addSubview(image)
-        
-        buttonView.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-        }
-        
-        image.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(5)
-            make.top.equalTo(buttonView.snp.top).offset(5)
-        }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.leading.equalTo(image.snp.trailing).offset(5)
-            make.top.equalTo(buttonView.snp.top).offset(5)
-        }
-        
-        descLabel.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().offset(5)
-            make.top.equalTo(titleLabel.snp.bottom).offset(5)
-        }
-    }
-    
-    @objc private func didTap() {
-        // Handle tap gesture
-    }
-}
+//public class ReportButton: UIView {
+//
+//    private let title: String
+//    private let reportButtonDescription: String
+//    private let systemNameIcon: String
+//
+//    // Designated initializer
+//    init(title: String, reportButtonDescription: String, systemNameIcon: String) {
+//        self.title = title
+//        self.reportButtonDescription = reportButtonDescription
+//        self.systemNameIcon = systemNameIcon
+//        super.init(frame: .zero) // You can set the frame here or customize it as needed
+//        setupUI()
+//        setupGesture()
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    private func setupGesture() {
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap))
+//        addGestureRecognizer(tapGesture)
+//        isUserInteractionEnabled = true
+//    }
+//
+//    private func setupUI() {
+//
+//        let buttonView = UIView()
+//
+//        addSubview(buttonView)
+//
+//        let image: UIImageView = {
+//            let icon = UIImageView()
+//            if #available(iOS 13.0, *) {
+//                icon.image = UIImage(systemName: systemNameIcon)
+//            }
+//            return icon
+//        }()
+//
+//        lazy var titleLabel = UILabel()
+//            .with(\.text, value: title)
+//            .with(\.textColor, value: UIColor.from(hex: "#bbbcbd"))
+//            .with(\.font, value: .systemFont(ofSize: 16, weight: .bold))
+//
+//        lazy var descLabel = UILabel()
+//            .with(\.text, value: reportButtonDescription)
+//            .with(\.textColor, value: UIColor.from(hex: "#bbbcbd"))
+//            .with(\.numberOfLines, value: 0)
+//            .with(\.font, value: .systemFont(ofSize: 12, weight: .light))
+//
+//        buttonView.addSubview(titleLabel)
+//        buttonView.addSubview(descLabel)
+//        buttonView.addSubview(image)
+//
+//        buttonView.snp.makeConstraints { make in
+//            make.leading.trailing.equalToSuperview()
+//        }
+//
+//        image.snp.makeConstraints { make in
+//            make.leading.equalToSuperview().offset(5)
+//            make.top.equalTo(buttonView.snp.top).offset(5)
+//        }
+//
+//        titleLabel.snp.makeConstraints { make in
+//            make.leading.equalTo(image.snp.trailing).offset(5)
+//            make.top.equalTo(buttonView.snp.top).offset(5)
+//        }
+//
+//        descLabel.snp.makeConstraints { make in
+//            make.leading.trailing.equalToSuperview().offset(5)
+//            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+//        }
+//    }
+//
+//    @objc private func didTap() {
+//        // Handle tap gesture
+//    }
+//}
 
 
 class DummyController: UIViewController {
@@ -118,6 +118,10 @@ class DummyController: UIViewController {
     
     private let imageView: UIImageView
     
+    private let reportButtonData = [
+        ("Report a bug", "Something in the app is broken or doesn't work as expected", "ladybug"),
+        ("Suggest an improvement", "New ideas or desired enhancements for this app", "megaphone")
+    ]
     
     init(imageView: UIImageView) {
         self.imageView = imageView
@@ -128,7 +132,7 @@ class DummyController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    
     let popupView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.from(hex: "#333333")
@@ -144,16 +148,6 @@ class DummyController: UIViewController {
         .with(\.font, value: .systemFont(ofSize: 18))
     
     
-    lazy var separator1 = UIView.separator(color: .white)
-    
-    lazy var separator2 = UIView.separator(color: .white)
-    
-    lazy var reportBugButton = ReportButton(title: "Report a bug", reportButtonDescription: "Something in the app is broken or doesn't work as expected", systemNameIcon: "ladybug")
-    
-    lazy var proposeEnhancementButton = ReportButton(title: "Suggest an improvement", reportButtonDescription: "New ideas or desired enhancements for this app", systemNameIcon: "megaphone")
-    
-    lazy var separator3 = UIView.separator(color: .white)
-    
     
     lazy var cancelButton: UIButton = {
         let button = UIButton()
@@ -162,19 +156,28 @@ class DummyController: UIViewController {
         return button
     }()
     
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .singleLine
+        return tableView
+    }()
+    
+    
+    
     @objc func closeButtonTapped() {
         dismiss(animated: true, completion: nil)
     }
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .clear
         modalPresentationStyle = .fullScreen
         
         view.addSubview(popupView)
-
+        
         
         popupView.snp.makeConstraints { make in
             make.center.equalToSuperview()
@@ -185,55 +188,73 @@ class DummyController: UIViewController {
         // Customize the appearance of the popup view
         configurePopupView()
     }
-
+    
     func configurePopupView() {
         popupView.addSubview(popupTitle)
-        popupView.addSubview(separator1)
-        popupView.addSubview(reportBugButton)
-        popupView.addSubview(proposeEnhancementButton)
-        popupView.addSubview(separator2)
-        popupView.addSubview(separator3)
         popupView.addSubview(cancelButton)
-    
+        
+        tableView.register(ReportButtonCell.self, forCellReuseIdentifier: "ReportButtonCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.backgroundColor = .clear
+        modalPresentationStyle = .fullScreen
+        
+        view.addSubview(tableView)
+        
+        
+        tableView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.edges.equalToSuperview()
+        }
+        
         
         popupTitle.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(5)
             make.leading.equalTo(popupView.snp.leading).offset(10)
         }
         
-        separator1.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(1)
-            make.top.equalTo(popupTitle.snp.bottom).offset(50)
-        }
-        
-        reportBugButton.snp.makeConstraints { make in
-            make.leading.trailing.equalTo(popupView).offset(5)
-            make.top.equalTo(separator1.snp.bottom).offset(5)
-        }
-        
-        separator2.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(1)
-            make.top.equalTo(reportBugButton.snp.bottom).offset(5)
-        }
-        
-        proposeEnhancementButton.snp.makeConstraints { make in
-            make.top.equalTo(separator2.snp.bottom).offset(2)
-            make.leading.trailing.equalTo(popupView).offset(5)
-        }
-        
-        separator3.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview()
-            make.height.equalTo(1)
-            make.top.equalTo(proposeEnhancementButton.snp.bottom).offset(50)
-        }
         
         cancelButton.snp.makeConstraints { make in
-            make.top.equalTo(separator3.snp.bottom).offset(2)
             make.centerX.equalTo(popupView.snp.centerX)
             make.bottom.equalTo(popupView.snp.bottom).inset(10)
         }
+    }
+}
+
+
+class ReportButtonCell: UITableViewCell {
+    
+    // ... existing code ...
+    
+    func configure(title: String, description: String, iconName: String) {
+        // ... update the cell UI based on the provided data ...
+    }
+}
+
+extension DummyController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reportButtonData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReportButtonCell", for: indexPath) as! ReportButtonCell
+        
+        let (title, description, iconName) = reportButtonData[indexPath.row]
+        cell.configure(title: title, description: description, iconName: iconName)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Handle cell selection, e.g., show a new view controller based on the selected report button
     }
 }
 
@@ -252,7 +273,7 @@ public struct MySwiftPackage {
 }
 
 public class ScreenshotObserver {
-
+    
     @objc static func detectScreenshot() {
         // Retrieve the top-most visible view controller
         if let topViewController = UIApplication.shared.keyWindow?.rootViewController?.itx_visibleViewController {
@@ -262,25 +283,25 @@ public class ScreenshotObserver {
                 if let jpegData = screenshot.jpegData(compressionQuality: 1.0) {
                     print("Captured screenshot with size: \(jpegData.count) bytes")
                 }
-
+                
                 // Create and present the DummyController
                 let controller = DummyController(imageView: UIImageView(image: screenshot))
                 
                 controller.modalPresentationStyle = .fullScreen
-
+                
                 topViewController.present(controller, animated: true)
             }
         }
     }
-
-
+    
+    
     static func captureScreen(view: UIView) -> UIImage? {
         // Capture the screen as an image
         UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
         view.layer.render(in: UIGraphicsGetCurrentContext()!)
         let screenshot = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-
+        
         return screenshot
     }
 }
