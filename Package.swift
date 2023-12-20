ccbabcd2131bf3af9c4cd942d431c26b46b9560a
Adrien Ronse <adrien.ronse@epitech.eu>
2023-12-20 17:28:04 +0100

    apollo package

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.6.0"),
-        .package(url: "https://github.com/adronse/iOS-gql", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/adronse/iOS-gql", .branch("main")),
     ],
     targets: [
         .target(
