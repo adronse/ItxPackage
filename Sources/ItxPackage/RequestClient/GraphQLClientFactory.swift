80c45d941b59fc5a32e5be87d12823eeb5a58ed4
Adrien Ronse <adrien.ronse@epitech.eu>
2024-01-05 11:54:59 +0100

    update ngrok

--- Sources/ItxPackage/RequestClient/GraphQLClientFactory.swift
+++ Sources/ItxPackage/RequestClient/GraphQLClientFactory.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class GraphQLClientFactory {
     static func createClient() -> GraphQLClient {
-        let url = URL(string: "https://d4c9-2a05-6e02-10d1-a710-959-3410-e847-4238.ngrok-free.app/graphql")!
+        let url = URL(string: "https://a082-2a05-6e02-10d1-a710-3ca9-7f62-932d-97f0.ngrok-free.app/graphql")!
         let apiKey = IterationX.shared.getApiKey()
         return GraphQLClient(url: url, apiKey: apiKey)
     }
