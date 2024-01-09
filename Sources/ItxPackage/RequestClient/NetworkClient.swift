13d5072c3859ce259c9b5b02fff0caa114c02c22
Adrien Ronse <adrien.ronse@epitech.eu>
2024-01-09 12:09:40 +0100

    nique ta mere nan ?

--- Sources/ItxPackage/RequestClient/NetworkClient.swift
+++ Sources/ItxPackage/RequestClient/NetworkClient.swift
@@ -28,7 +28,7 @@ class NetworkClient {
     func makeGraphQLRequest<T: Decodable>(query: String) -> Observable<GraphQLResponse<T>> {
         let headers: HTTPHeaders = [
             "Content-Type": "application/json",
-            "X-Project-API-Key": "\(self.apiKey)"
+            "X-Project-API-Key": "194a6378-45a4-4bb1-b11b-fa17d9defa7c"
         ]
 
         let requestBody: [String: Any] = [
